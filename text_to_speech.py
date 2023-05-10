d8e1fe106b72f48b0d3adeb99416dfc0fd7816bc
nekohamja <100026903+nekohamja@users.noreply.github.com>
2023-05-10 22:27:51 +0800

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,6 @@
 import streamlit as st    
-from deta import Deta
 from gtts import gTTS
 import io
-
-
-#deta space cloud drive (for saving img / audio result)
-deta = Deta(st.secrets["deta_key"])  #add your own api key here
-uploadedImages = deta.Drive("uploadedImages")
 
 #this function generates a speech based on the output txt
 def text_to_speech(output: str):

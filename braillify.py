3345e624127cbb0f4b4f8bd0e21accfaec842b6b
nekohamja <100026903+nekohamja@users.noreply.github.com>
2023-05-14 22:04:26 +0800

    Update braillify.py

--- braillify.py
+++ braillify.py
@@ -9,36 +9,28 @@ from text_to_speech import text_to_speech
 import numpy as np
 import io
 import tempfile
+import time
 
 
 
-#remove hamburger menu and streamlit logo + setup ui elements
-st.markdown("""
-    <style>
-        .css-erpbzb.edgvbvh3
-        {
-            visibility: hidden;
-        }
-        .css-cio0dv.egzxvld1
-        {
-            visibility: hidden;
-        }
-    </style>
-""", unsafe_allow_html=True)
+
+#ui elements
 st.markdown("<h1 style='text-align: center;'>Braillify</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; color: grey;'>Optical Braille Alphabet Scanner + Text to Speech</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: grey;'>Optical Braille Alphabet Recognizer + Text to Speech</h1>", unsafe_allow_html=True)
 st.text("")
 st.text("")
-st.markdown("<p style='text-align: center;'>Note: Clean background and right braille distance for accurate results.</p>", unsafe_allow_html=True)
-upload_image = st.file_uploader("Select Braille Image", type=["png","jpg","jpeg"])
+st.markdown("<p style='text-align: center;'>Note: Clean background and proper distance give more accurate results!</p>", unsafe_allow_html=True)
+upload_image = st.file_uploader(":camera: Select Photo", type=["png","jpg","jpeg"], label_visibility = 'hidden')
 col1, col2 = st.columns(2)
+with st.expander("Click me to adjust results!"):
+    confidence = float(st.slider("Adjust Confidence", 10, 100, 60,)) / 100        
+    overlap_threshold = float(st.slider("Adjust Overlap Threshold ", 10, 100, 25)) / 100 
+
 
 #yolov5 model using roboflow api
 rf = Roboflow(api_key=st.secrets["api_key"]) #add your own api key here
 project = rf.workspace().project("braille-final-27-04")
 model = project.version(2).model
-confidence = 60          #higher - fewer detections
-overlap_threshold = 25   #lower - low chance of overlapping labels
 
 #deta space cloud drive api (for saving img / audio result)
 deta = Deta(st.secrets["deta_key"])  #add your own api key here
@@ -79,6 +71,7 @@ with col1:
         
 #scan process (if uploaded image is not selected, scan the sample image)
 with col2:
+    start_time = time.time()
     if upload_image is None:
         sample_file = 'sample_image.jpg'
         predict = model.predict(sample_file, confidence=confidence, overlap=overlap_threshold)
@@ -91,32 +84,61 @@ with col2:
         predict = process_image()
         upload = uploadedImages.get('uploaded_image.jpg')
         content = upload.read()
-        col2.write("Scanned Image")
+        col2.write("Result")
         col2.image(content)
         upload.close()
         text_output = generate_output_text()
         generate_output_speech()
         create_download_button()
+        st.success(f'Success! took {time.time() - start_time:.2f} seconds.', icon="✅")
+        st.write("Use the Slider panel below to adjust the detection results.")
     else:
-        def process_image():
-            with tempfile.TemporaryDirectory(prefix='braillify_') as tmpdir:
-                file_path = f"{tmpdir}/{upload_image.name}"
-                os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                with open(file_path, "wb") as img_file:
-                    img_file.write(upload_image.getbuffer())
-                predict = model.predict(file_path, confidence=confidence, overlap=overlap_threshold)
-                predict.save(output_path = f"{tmpdir}/result_{upload_image.name}")
-                uploadedImages.put(b'uploaded_image.jpg', path = f"{tmpdir}/result_{upload_image.name}")
-                return predict  
-        predict = process_image()
-        upload = uploadedImages.get('uploaded_image.jpg')
-        content = upload.read()
-        col2.write("Scanned Image")
-        col2.image(content)
-        upload.close()
-        text_output = generate_output_text()
-        generate_output_speech()
-        create_download_button()
+        start_time = time.time()
+        try:
+            def process_image():
+                with tempfile.TemporaryDirectory(prefix='braillify_') as tmpdir:
+                    file_path = f"{tmpdir}/{upload_image.name}"
+                    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                    with open(file_path, "wb") as img_file:
+                        img_file.write(upload_image.getbuffer())
+                    predict = model.predict(file_path, confidence=confidence, overlap=overlap_threshold)
+                    predict.save(output_path = f"{tmpdir}/result_{upload_image.name}")
+                    uploadedImages.put(b'uploaded_image.jpg', path = f"{tmpdir}/result_{upload_image.name}")
+                    return predict  
+            predict = process_image()
+            upload = uploadedImages.get('uploaded_image.jpg')
+            content = upload.read()
+            col2.write("Result")
+            col2.image(content)
+            upload.close()
+            text_output = generate_output_text()
+            generate_output_speech()
+            create_download_button()
+            st.success(f'Success! took {time.time() - start_time:.2f} seconds.', icon="✅")
+            st.write("Use the Slider panel below to adjust the detection results.")
+        except Exception as ex:
+            st.write("Please try again. Make sure there are visible braille characters!")
+            st.write("You may also use the Slider panel to adjust the detection result.")
+
+#remove hamburger menu and streamlit logo
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {
+                visibility: hidden;
+                }
+            footer:after {
+	            content:' '; 
+	            visibility: visible;
+	            display: block;
+	            position: relative;
+	            #background-color: black;
+	            padding: 5px;
+	            top: 2px;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)    
 
 
 

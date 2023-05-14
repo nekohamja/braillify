c0f6c8e5e0c7f622f44ffd924cbc6cfc9fef335b
nekohamja <100026903+nekohamja@users.noreply.github.com>
2023-05-14 19:15:54 +0800

    Update arrange_boxes.py

--- arrange_boxes.py
+++ arrange_boxes.py
@@ -24,6 +24,7 @@ def sort_letters(predict_json: json):
 
     #sort y coordinate
     def sortY(val): return val[1]
+    def sortX(val): return val[0]
     pred_shape.sort(key= sortY)
 
     #line break/space threshold
@@ -31,46 +32,55 @@ def sort_letters(predict_json: json):
  
     y_diff = np.array(preds_y)
     y_diff.sort()
-    y_threshold = np.mean(arr[:, 3], dtype = 'float') // 2
+    y_threshold = np.mean(preds_h, dtype = float) // 2
     boxes_y_diff = np.diff(y_diff)
     y_threshold_index = np.where(boxes_y_diff > y_threshold)[0]
 
-    # cluster according to threshold_index
-    boxes_clustered = np.array_split(arr, y_threshold_index + 1)
-    boxes_return = []
-    for cluster in boxes_clustered:
-        # sort x coordinate
-        cluster = cluster[cluster[:, 0].argsort()]
-        boxes_return.append(cluster)
 
-    #iterate the labels through the 3d array
-    labels = []
-    for j in range(len(boxes_return)):
-        labels.append(' ')
-        for k in range(len(boxes_return[j])):
-            count=0
-            for l in range(len(boxes_return[j][k])):
-                count+=1
-                if count == 5:
-                    labels.append(boxes_return[j][k][l])
+    #check if img is sentence or paragraph
+    if len(y_threshold_index) == 0:
+        pred_shape.sort(key= sortX)
+        arr = np.array(pred_shape)
+        sorted_list = arr.tolist()
+        labels = [x.pop(4) for x in sorted_list]
+        final_result = ("".join(labels))
+    else:
+        # cluster/ split array according to threshold_index
+        boxes_clustered = np.array_split(arr, y_threshold_index + 1)
+        boxes_return = []
+        count = -1
+        x_diff = []
+        for cluster in boxes_clustered:
+            #sort x coordinate
+            cluster = cluster[cluster[:, 0].argsort()]
+            boxes_return.append(cluster)
 
-
-
-    #objectives: fix 'x' argsort misplacement, add 'x' diff and threshold index for spaces
-    #argsort fails when a word is more than 6 characters / or somethings blocking the bg of braille space
-
-    #test
-    #print('-----x-----y---width---height--label')
-    #print(arr)
-    #print('y sorted values',y_diff)
-    #print('y threshold:' , y_threshold)
-    #print('difference: ', boxes_y_diff)
-    #print('threshold index: ', y_threshold_index)
-    #print('boxes_return \n', boxes_return)
-    #print('-------------------------')
+            # get the x difference
+            count += 1
+            x = len(cluster)
+            for a in range(x):
+                x_diff.append(boxes_return[count][a][0])
 
 
 
-    #output the string result
-    final_result = ("".join(labels))
+        #--WORK IN PROGRESS--- (SPLITING SPACES)
+        x_diff = np.array(x_diff, dtype = float)
+        boxes_x_diff = np.diff(x_diff)
+        x_threshold = np.mean(preds_w, dtype = float) // 2
+
+
+
+        #iterate the labels through the 3d array
+        labels = []
+        for j in range(len(boxes_return)):
+            labels.append(' ')
+            for k in range(len(boxes_return[j])):
+                count=0
+                for l in range(len(boxes_return[j][k])):
+                    count+=1
+                    if count == 5:
+                        labels.append(boxes_return[j][k][l])
+
+        #output the string result
+        final_result = ("".join(labels))
     return final_result

7187edb852ff2d8d728d4b94169883116c0acb51
nekohamja <100026903+nekohamja@users.noreply.github.com>
2023-05-16 18:37:12 +0800

    Update remove_streamlit_logo.py

--- remove_streamlit_logo.py
+++ remove_streamlit_logo.py
@@ -18,6 +18,14 @@ def remove_streamlit_logo():
                     top: 2px;
                 }
                 header {visibility: hidden;}
+                [data-testid="column"] {
+                    box-shadow: rgb(0 0 0 / 20%) 0px 10px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+                    border-radius: 8px;
+                    padding: 1% 1% 1% 1%;
+                    background-color: #212F3D;
+                } 
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)    
+
+    
